ed6c58a7fa829e24ed2789678d96367bb53013af
Grayson Whitaker <glwhitaker3@gmail.com>
2023-03-08 17:23:54 -0500

    gitignore

--- Lecture/Review/Review 3/testerMain.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "review.h"
-
-int main(){
-    ReadStdIn2();
-
-    return 0;
-}
--- Lecture/Review/Review 7/testerMain.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "review.h"
-
-int main(){
-    ReadWrite();
-
-    return 0;
-}
